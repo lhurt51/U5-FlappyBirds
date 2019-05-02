45800bb9399a968e015e9fc437f87eaffe79303a
Liam Hurt <liamhurt51@gmail.com>
2019-05-01 19:35:51 -0700

    Adding Animation states along with cleaning up the bird coding and comenting it

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,33 +4,41 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-
+    // Up force of players input (defined in editor)
     public float upForce = 200.0f;
 
+    // Checks if the player is dead
     private bool bIsDead = false;
+    // Storing a copy of our rigid body component
     private Rigidbody2D rigidBody2D;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Making sure we have a rigid body component and storing it
         rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (bIsDead == false)
+        // If bird is dead end update
+        if (bIsDead == true) return;
+
+        // If User presses space
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rigidBody2D.velocity = Vector2.zero;
-                rigidBody2D.AddForce(new Vector2(0, upForce));
-            }
+            // Make sure zero the velocity for clean interpolation
+            rigidBody2D.velocity = Vector2.zero;
+            // Adding an upward force of up force
+            rigidBody2D.AddForce(new Vector2(0, upForce));
         }
     }
 
+    // Unity's Collision overide
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // Setting the bird to dead
         bIsDead = true;
     }
 }
